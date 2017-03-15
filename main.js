806f5b789e34e02c2c508e68d11ea0041d0a7c56
April Blair <a.m.blair@eagle.clarion.edu>
2017-03-15 19:09:06 -0400

    added checkForWinner function

--- main.js
+++ main.js
@@ -36,6 +36,23 @@ function switchTurn()
 	setMessage("It's " + document.turn + "'s turn.'")
 }
 
+function checkForWinner(move)
+{
+	var result = false;
+	if(checkRow(1, 2, 3, move) ||
+	  (checkRow(4, 5, 6, move) ||
+	  (checkRow(7, 8, 9, move) ||
+	  (checkRow(1, 4, 7, move) ||
+	  (checkRow(2, 5, 8, move) ||
+	  (checkRow(3, 6, 9, move) ||
+	  (checkRow(1, 5, 9, move) ||
+	  (checkRow(3, 5, 7, move)
+	  	{
+	  		result = true; 
+		}
+		return result;
+}
+
 //compares values from boxes to specific rows that we pass in.
 function checkRow(a, b, c, move)
 {

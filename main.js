a459eb88c40690a94ad1bd61f0752bb0bd168a01
April Blair <a.m.blair@eagle.clarion.edu>
2017-03-15 18:54:44 -0400

    first commit

---
+++ main.js
@@ -0,0 +1,47 @@
+function startGame() 
+{
+	document.turn = "X";
+	setMessage(document.turn + " get's to start.")
+}
+
+function setMessage(msg)
+{
+	document.getElementById("message").innerText = msg;
+
+}
+
+function nextMove(square) 
+{
+	if(square.innerText == '')
+	{
+		square.innerText = document.turn;
+		switchTurn();
+	}
+		else
+		{
+			setMessage("Pick another square.")
+		}
+}
+
+function switchTurn() 
+{
+	if(document.turn == "X") 
+	{
+		document.turn = "O";
+	}
+		else 
+		{
+			document.turn = "X"; 
+		}
+	setMessage("It's " + document.turn + "'s turn.'")
+}
+
+function checkRow(a, b, c, move)
+{
+
+}
+
+function getBox(number)
+{
+	
+}

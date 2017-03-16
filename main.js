ac69e2956cd0feaa7afe7a11736b8e28c9351522
April Blair <a.m.blair@eagle.clarion.edu>
2017-03-15 21:24:18 -0400

    debugging

--- main.js
+++ main.js
@@ -1,75 +1,156 @@
-function startGame() 
+//function startGame() {
+//	document.turn = "X";
+//	document.winner = null;
+//	setMessage(document.turn + " get's to start.")
+//}
+
+//function setMessage(msg) {
+//	document.getElementById("message").innerText = msg;
+//}
+
+//function nextMove(square) {
+//	if (document.winner != null) {
+	//	setMessage(document.turn + " already won.")
+	//} else if(square.innerText == '') {
+//		square.innerText = document.turn;
+//		switchTurn();
+//	} else {
+//	  setMessage("Pick another square.")
+//	}
+//}
+
+//function switchTurn() {
+//	if(checkForWinner(document.turn)) {
+//		setMessage("Congrats " + document.turn + " , you won!")
+//		document.winner = document.turn;
+//} else if(document.turn == "X") {
+//		document.turn = "O";
+// 		setMessage("It's " + document.turn + "'s turn.")
+// 		} else 
+// 		{
+// 			document.turn = "X"; 
+// 			setMessage("It's " + document.turn + "'s turn.")
+// 		}
+	
+// }
+
+//checks each row for input that matches the move. 
+// function checkForWinner(move) {
+// 	var result = false;
+// 	if(checkRow(1, 2, 3, move) ||
+// 	  (checkRow(4, 5, 6, move) ||
+// 	  (checkRow(7, 8, 9, move) ||
+// 	  (checkRow(1, 4, 7, move) ||
+// 	  (checkRow(2, 5, 8, move) ||
+// 	  (checkRow(3, 6, 9, move) ||
+// 	  (checkRow(1, 5, 9, move) ||
+// 	  (checkRow(3, 5, 7, move)) {
+// 	  	result = true; 
+// 	  }	
+// 		return result;
+// }
+
+//compares values from boxes to specific rows that we pass in.
+// function checkRow(a, b, c, move)
+// {
+// 	var result = false;
+// 	if (getBox(a) == move && getBox(b) == move && getBox(c) == move)
+// 		{
+// 			result = true;
+// 		}
+// 		return result;
+// }
+
+//gets the values from the boxes.
+// function getBox(number)
+// {
+// 	return document.getElementById("s" + number).innerText;
+// }
+
+// function clearBox(number) {
+// 	document.getElementById("s" + number).innerText
+// }
+
+function startGame()
 {
 	document.turn = "X";
+	document.winner = null;
 	setMessage(document.turn + " get's to start.")
 }
 
 function setMessage(msg)
 {
 	document.getElementById("message").innerText = msg;
-
 }
 
-function nextMove(square) 
+function nextMove(square)
 {
-	if(square.innerText == '')
+	if (document.winner != null)
 	{
-		square.innerText = document.turn;
-		switchTurn();
+	setMessage(document.turn + " already won.")
 	}
-		else
-		{
-			setMessage("Pick another square.")
-		}
+	else if(square.innerText == '')
+	{
+	square.innerText = document.turn;
+	switchTurn();
+	}
+	else
+	{
+	setMessage("Pick another square.")
+	}
 }
 
-function switchTurn() 
+function switchTurn()
 {
 	if(checkForWinner(document.turn))
-		{
-			setMessage("Congrats " + document.turn + " , you won!")
-		}
-	else if(document.turn == "X") 
 	{
-		document.turn = "O";
-		setMessage("It's " + document.turn + "'s turn.'")
+	setMessage("Congrats " + document.turn + ", you won!")
+	document.winner = document.turn;
 	}
-		else 
-		{
-			document.turn = "X"; 
-			setMessage("It's " + document.turn + "'s turn.'")
-		}
-	
+	else if(document.turn == "X")
+	{
+	setMessage("It's " + document.turn + "'s turn.")
+	}
+	else
+	{
+	document.turn = "X";
+	setMessage("It's " + document.turn + "'s turn.")
+	}
 }
 
-// checks each row for input that matches the move. 
 function checkForWinner(move)
 {
 	var result = false;
 	if(checkRow(1, 2, 3, move) ||
-	  (checkRow(4, 5, 6, move) ||
-	  (checkRow(7, 8, 9, move) ||
-	  (checkRow(1, 4, 7, move) ||
-	  (checkRow(2, 5, 8, move) ||
-	  (checkRow(3, 6, 9, move) ||
-	  (checkRow(1, 5, 9, move) ||
-	  (checkRow(3, 5, 7, move)
-	  	{
-	  		result = true; 
-		}
-		return result;
+	(checkRow(4, 5, 6, move) ||
+	(checkRow(7, 8, 9, move) ||
+	(checkRow(1, 4, 7, move) ||
+	(checkRow(2, 5, 8, move) ||
+	(checkRow(3, 6, 9, move) ||
+	(checkRow(1, 5, 9, move) ||
+	(checkRow(3, 5, 7, move) ||
+	{
+	result = true;
+	}
+	return result;
 }
 
-//compares values from boxes to specific rows that we pass in.
 function checkRow(a, b, c, move)
 {
 	var result = false;
 	if (getBox(a) == move && getBox(b) == move && getBox(c) == move)
-		result = true;
+	{
+	result = true;
+	}
+	return result;
 }
-
-//gets the values from the boxes.
+ 
 function getBox(number)
 {
 	return document.getElementById("s" + number).innerText;
+}
+
+function clearBox(number)
+{
+	document.getElementById("s" + number).innerText = "";
 }
